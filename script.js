da025c013fb08ba61afd86839ff02e5d2c29227e
Scarlett Eller <rq6715@gmail.com>
2022-01-24 17:15:15 -0500

    day 1 edits

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
-const h2 = document.createElement('h2')
-    h2.textContent = "This is added by Javascript"
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('http://localhost:3000/movies/4')
+        .then(res => res.json())
+        .then(data1 => {
+            displayFirstMovie(data1)
+        })
+})
 
-const body = document.querySelector("body")
-body.appendChild(h2)
+function displayFirstMovie(movie1) {
+    let newTitle = document.querySelector('#title')
+        newTitle.textContent = `${movie1.title}`
+    let newGenre = document.querySelector('#genre')
+        newGenre.textContent = `${movie1.genre}`
+    let newDescription = document.querySelector('#description')
+        newDescription.textContent = `${movie1.description}`
+    let newUser = document.querySelector('#user')
+        newUser.textContent = `${movie1.recommendedBy}`
+    let newRec = document.querySelector('#recommendation')
+        newRec.textContent = `${movie1.recommendation}`
+    let newPoster = document.querySelector('#bigPoster')
+        newPoster.src = `${movie1.poster}`
+    let newWatches = document.querySelector('#number')
+        newWatches.textContent = `${movie1.watches}`
+
+    const watchedButton = document.querySelector('#watchedButton')
+    watchedButton.addEventListener('click', () => watchedMovie(movie1))
+    
+}
+
+function watchedMovie(movie) {
+    let watchCount = document.querySelector('#number')
+    watchCount.textContent = movie.watches + 1
+    watchedButton.remove()
+
+}
